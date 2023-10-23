ac553cead4eab5ddde416509d03922ad4750ebcf
niiis <niiis>
2023-10-23 17:52:33 +0900

    first commist

---
+++ source/conf.py
@@ -0,0 +1,29 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'cnmsa_level1'
+copyright = '2023, Seiya'
+author = 'Seiya'
+release = 'release-0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'ja'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# sphinx_rtd_themeの設定
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ source/index.rst
@@ -0,0 +1,20 @@
+.. cnmsa_level1 documentation master file, created by
+   sphinx-quickstart on Mon Oct 23 16:56:16 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to cnmsa_level1's documentation!
+========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
